18502a128a6ff457fab4dfd64abfd9530ebce062
Rutik <rutikkulkarni2001@gmail.com>
2024-10-09 12:45:19 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello!</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
